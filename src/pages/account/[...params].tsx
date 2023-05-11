240cf649ec4e215093b2274ed6ab04b33adf599d
Alexandria65r <robertchingambu65@gmail.com>
2023-05-11 03:13:42 +0200

    isfixed?

--- src/pages/account/[...params].tsx
+++ src/pages/account/[...params].tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import Layout from '../../components/layout'
 import { Box, styled } from '@mui/material'
 import { useAppSelector } from '../../../store/hooks'
-import InfoItem from './info-item'
+import InfoItem from '../../components/account/info-item'
 import { RiContactsLine } from 'react-icons/ri'
 import { MdOutlineAddAPhoto, MdPayment } from 'react-icons/md'
 import { FaGraduationCap } from 'react-icons/fa'
-import Header from './header'
-import AccountLinks from './links'
 import { useRouter } from 'next/router'
-import ProfilePhoto from './profile-photo'
-import PrimaryInfo from './primary-info'
-import RoleInfo from './role-info'
-import PaymentMethods from './payment-methods'
+import AccountLinks from '../../components/account/links'
+import PaymentMethods from '../../components/account/payment-methods'
+import PrimaryInfo from '../../components/account/primary-info'
+import ProfilePhoto from '../../components/account/profile-photo'
+import RoleInfo from '../../components/account/role-info'
+
 
 
 
@@ -27,7 +27,7 @@ export default function Profile({ }: Props) {
   console.log(router.query)
   return (
     <>
-      {/* {params[0] === user?._id ? <AccountLinks /> : <></>} */}
+      {params[0] === user?._id ? <AccountLinks /> : <></>}
       {params[0] === 'profile-photo' ? <ProfilePhoto /> : <></>}
       {params[0] === 'primary-info' ? <PrimaryInfo /> : <></>}
       {params[0] === 'role-info' ? <RoleInfo /> : <></>}
