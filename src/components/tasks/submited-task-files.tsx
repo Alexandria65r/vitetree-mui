54bc13852236268ae7aff2df6c61dceb0b5e0050
Alexandria65r <robertchingambu65@gmail.com>
2023-11-13 14:09:15 +0200

    preview update

--- src/components/tasks/submited-task-files.tsx
+++ src/components/tasks/submited-task-files.tsx
@@ -1,11 +1,15 @@
 import { styled, Box, Typography, LinearProgress, useTheme, colors } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../../../store/hooks'
 import { colorScheme } from '../../theme'
-import { ButtonIcon } from '../../reusable/styles'
+import { ButtonIcon, StyledButton, Textarea } from '../../reusable/styles'
 import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
 import FileItem from './file-item'
 import { downloadTaskFileThunk } from '../../../reducers/tasks-reducer/task-thunks'
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+
+
+
 const Container = styled(Box)(() => ({
 
 }))
@@ -43,12 +47,15 @@ const RateTutor = styled(Box)(({ theme }) => ({
 
 }))
 
-const RatingItem = styled(Box)(({ theme }) => ({
+const RatingItem = styled(StyledButton)(({ theme }) => ({
+  height: 30,
   padding: 4,
   cursor: 'pointer',
   borderRadius: '29px',
   transition: '0.3s all',
   whiteSpace: 'nowrap',
+  backgroundColor: 'transparent',
+  color: colorScheme(theme).TextColor,
   border: `1px solid ${colorScheme(theme).borderColor}`,
   '&:hover': {
     color: colors.orange[500],
@@ -64,7 +71,18 @@ export default function SubmitedTaskFiles({ }: Props) {
   const _theme = useTheme()
   const dispatch = useAppDispatch()
   const task = useAppSelector((state) => state.TaskReducer.task)
-  const ratings = ['🙂Happy', '😍 Very happy', '☹️ Unhappy']
+  const [rating, setRating] = useState<{name:string; description:string}>({
+    name: '',
+    description:''
+  })
+ 
+  const ratings = ['🙂 Happy', '😍 Very happy', '☹️ Unhappy', '🙄 Poor']
+
+
+
+  function submitRating () {
+    console.log(rating)
+  }
   return (
     <Container>
 
@@ -82,16 +100,39 @@ export default function SubmitedTaskFiles({ }: Props) {
 
       <RateTutor>
         <Box sx={(theme) => ({ p: 1, borderBottom: `1px solid ${colorScheme(theme).borderColor}` })}>
-          <Text sx={{ fontWeight: 500, fontSize: '16px', lineHeight: '1.2' }}>Describe your satisfaction by the service offered.</Text>
+          <Text sx={{ fontWeight: 500, fontSize: '16px', lineHeight: '1.2' }}>Describe your satisfaction by the service delivered to you.</Text>
         </Box>
         <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, py: 2 }}>
           {ratings.map((item) => (
             <RatingItem key={item}
-              sx={(theme) => ({})}>
-              <Text> {item}</Text>
+              onClick={() => setRating({...rating, name:item})}
+              sx={(theme) => ({ borderColor: rating?.name === item ? colors.orange[500] : '' })}>
+              <Text sx={{ fontSize: '14px' }}>{item}</Text>
             </RatingItem>
           ))}
         </Box>
+        {rating && (
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
+            <Text sx={{ flexBasis: '100%', fontSize: 13, fontWeight: 600 }}>Description</Text>
+            <Box sx={{ flex: 1 }}>
+              <Textarea
+                value={rating.description}
+                onChange={({ target }) => setRating({...rating, description:target.value})}
+                sx={{ width: '100%' }}
+                placeholder='Brief discription about your choice..'
+              />
+            </Box>
+            <ButtonIcon
+              onClick={submitRating}
+            sx={{
+              backgroundColor: rating.description ? colors.teal[500] : '',
+              color: rating.description ? '#fff' : ''
+            }}>
+              <SendOutlinedIcon />
+            </ButtonIcon>
+          </Box>
+        )}
+
       </RateTutor>
     </Container>
   )
