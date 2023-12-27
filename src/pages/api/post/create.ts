68c5c150fbab4ee6066d8c3f3edd42f1d13a1a0c
Alexandria65r <robertchingambu65@gmail.com>
2023-12-27 17:29:39 +0200

    fix 05

--- src/pages/api/post/create.ts
+++ src/pages/api/post/create.ts
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Page } from '../../../models/page/page.model'
-import { PostModel } from '../../../models/post'
+import { JobModel } from '../../../models/post'
 
 async function createPost(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const newPost: Page = await PostModel.create(req.body)
+        const newPost: Page = await JobModel.create(req.body)
 
         if (newPost) {
             return res.json({ success: true, newPost })
